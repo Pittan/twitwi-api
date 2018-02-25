1226f34cd44d236565c727434252c7ad5aa241b4
Amon Keishima <amon.keishima@gmail.com>
2018-02-25 14:18:30 +0900

    ✨ CORSを有効にした

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ var app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+// Crossを有効
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+  next()
+})
+
+// Optionsも必要
+app.options('*', (req, res) => {
+  res.sendStatus(200)
+})
+
 // root
 app.get('/', function(request, response) {
   response.json({
